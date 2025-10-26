f25520ecb39eed8e3f64240c75768db0c6733175
Shinyepo <shinyepo42@gmail.com>
2025-10-26 23:37:31 +0100

    Add background color to statics

--- src/main/java/dev/shinyepo/resourcegenerator/menus/types/GuiElement.java
+++ src/main/java/dev/shinyepo/resourcegenerator/menus/types/GuiElement.java
@@ -1,6 +1,7 @@
 package dev.shinyepo.resourcegenerator.menus.types;
 
 public class GuiElement {
+    public static final int BACKGROUND = 0xFFC6C6C6;
     public static final int BASIC = 0xFF404040;
     public static final int RED = 0xFF800000;
 }
