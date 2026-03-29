6e176fc3a81912bc8e42768f0936037e6edc3b13
shiny <shinyepo42@gmail.com>
2026-03-29 14:50:46 +0200

    Change BasicConsumer model

--- src/main/java/dev/shinyepo/resourcegenerator/blocks/BasicConsumer.java
+++ src/main/java/dev/shinyepo/resourcegenerator/blocks/BasicConsumer.java
@@ -8,7 +8,6 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.phys.shapes.Shapes;
 
 public class BasicConsumer extends NetworkBlock {
     private static final BooleanProperty OPERATIONAL = CustomProperties.OPERATIONAL;
@@ -16,20 +15,7 @@ public class BasicConsumer extends NetworkBlock {
     public BasicConsumer(Properties properties) {
         super(BasicConsumerEntity::new, properties);
 
-        SHAPE = Shapes.or(Block.box(7, 7, 7, 9, 9, 9),
-                Block.box(6, 6, 6, 10, 10, 10),
-                Block.box(0, 0, 0, 16, 1, 16),
-                Block.box(1, 1, 1, 15, 2, 15),
-                Block.box(2, 2, 2, 14, 3, 14),
-                Block.box(3, 3, 3, 13, 4, 13),
-                Block.box(3.25, 4, 3.25, 4.75, 5, 4.75),
-                Block.box(10.9, 5, 3.7, 11.9, 6.2, 4.7),
-                Block.box(11.25, 4, 11.25, 12.75, 5, 12.75),
-                Block.box(10.9, 5, 10.5, 11.9, 6.2, 11.5),
-                Block.box(11.25, 4, 3.25, 12.75, 5, 4.75),
-                Block.box(4, 5, 10.5, 5, 6.2, 11.5),
-                Block.box(3.25, 4, 11.25, 4.75, 5, 12.75),
-                Block.box(5, 5, 3.7, 5, 6.2, 4.7));
+        SHAPE = Block.box(0, 0, 0, 16, 16, 16);
 
         registerDefaultState(getStateDefinition().any()
                 .setValue(OPERATIONAL, false));
