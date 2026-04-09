cecad2ad49acf57b716fca6a7719e0eba3df77b4
shiny <shinyepo42@gmail.com>
2026-04-09 19:12:37 +0200

    Bandage fix for null cap on render thread while connecting ItemPipe to ConsumerOutput

--- src/main/java/dev/shinyepo/resourcegenerator/blocks/entities/ConsumerOutputEntity.java
+++ src/main/java/dev/shinyepo/resourcegenerator/blocks/entities/ConsumerOutputEntity.java
@@ -2,6 +2,7 @@ package dev.shinyepo.resourcegenerator.blocks.entities;
 
 import dev.shinyepo.resourcegenerator.blocks.entities.types.ConsumerStructureEntity;
 import dev.shinyepo.resourcegenerator.registries.BlockEntityRegistry;
+import dev.shinyepo.resourcegenerator.util.ItemStacksHandlerUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
@@ -10,20 +11,24 @@ import net.neoforged.neoforge.capabilities.BlockCapabilityCache;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.transfer.ResourceHandler;
 import net.neoforged.neoforge.transfer.item.ItemResource;
+import net.neoforged.neoforge.transfer.item.ItemStacksResourceHandler;
 import org.jspecify.annotations.Nullable;
 
 public class ConsumerOutputEntity extends ConsumerStructureEntity {
     private BlockCapabilityCache<ResourceHandler<ItemResource>, @Nullable Direction> consumerItemCache;
+    private ItemStacksResourceHandler output;
 
     public ConsumerOutputEntity(BlockPos blockPos, BlockState blockState) {
         super(BlockEntityRegistry.CONSUMER_OUTPUT_ENTITY.get(), blockPos, blockState);
+
+        output = ItemStacksHandlerUtil.createOutputOnlyHandler(1, this::setChanged);
     }
 
     @Override
     public void assignConsumer(BlockPos consumerPos) {
         super.assignConsumer(consumerPos);
         assert this.level != null;
-        if (level.isClientSide()) return;
+//        if (level.isClientSide()) return;
 
         consumerItemCache = BlockCapabilityCache.create(
                 Capabilities.Item.BLOCK,
@@ -33,6 +38,7 @@ public class ConsumerOutputEntity extends ConsumerStructureEntity {
     }
 
     public ResourceHandler<ItemResource> getItemCapability(@Nullable Direction direction) {
-        return this.consumerItemCache.getCapability();
+        return output;
+//        return this.consumerItemCache.getCapability();
     }
 }
