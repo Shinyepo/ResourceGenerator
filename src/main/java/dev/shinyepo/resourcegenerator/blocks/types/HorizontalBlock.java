e3c255b138570e5b2a0abbc4afe0d9368983214e
shiny <shinyepo42@gmail.com>
2026-04-19 14:06:53 +0200

    Add HorizontalBlock for basic horizontal blocks

---
+++ src/main/java/dev/shinyepo/resourcegenerator/blocks/types/HorizontalBlock.java
@@ -0,0 +1,50 @@
+package dev.shinyepo.resourcegenerator.blocks.types;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HorizontalBlock extends BasicBlock {
+    protected static final EnumProperty<Direction> FACING = BlockStateProperties.HORIZONTAL_FACING;
+    protected final Map<Direction, VoxelShape> SHAPES = new HashMap<>();
+
+    public HorizontalBlock(Properties properties) {
+        super(properties);
+
+        registerDefaultState(getStateDefinition().any()
+                .setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected @NonNull VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPES.get(state.getValue(FACING));
+    }
+
+    @Override
+    protected @NonNull VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPES.get(state.getValue(FACING));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public @Nullable BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+}
