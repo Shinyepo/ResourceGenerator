57eb37d798e404712dc2387d5a1ff75dc28280c6
shiny <shinyepo42@gmail.com>
2026-04-27 14:07:42 +0200

    Refactor ControllerSummaryTab to use new Widgets and cleanup/switch logic.

--- src/main/java/dev/shinyepo/resourcegenerator/menus/controller/tabs/ControllerSummaryTab.java
+++ src/main/java/dev/shinyepo/resourcegenerator/menus/controller/tabs/ControllerSummaryTab.java
@@ -5,6 +5,11 @@ import dev.shinyepo.resourcegenerator.menus.controller.ControllerContainer;
 import dev.shinyepo.resourcegenerator.menus.controller.ControllerScreen;
 import dev.shinyepo.resourcegenerator.menus.types.GuiElement;
 import dev.shinyepo.resourcegenerator.menus.types.ScreenTab;
+import dev.shinyepo.resourcegenerator.menus.widgets.AbstractAccountWidget;
+import dev.shinyepo.resourcegenerator.menus.widgets.BalanceWidget;
+import dev.shinyepo.resourcegenerator.menus.widgets.ChangeWidget;
+import dev.shinyepo.resourcegenerator.menus.widgets.OwnerWidget;
+import dev.shinyepo.resourcegenerator.util.GuiNumericUtil;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.Component;
@@ -18,8 +23,45 @@ public class ControllerSummaryTab extends ScreenTab<ControllerContainer, Control
     private static final Identifier CARD_SLOT = fromNamespaceAndPath(ResourceGenerator.MODID, "textures/gui/controller/tabs/card_slot.png");
     private static final Identifier INVENTORY_SLOTS = fromNamespaceAndPath(ResourceGenerator.MODID, "textures/gui/controller/tabs/inventory_slots.png");
 
+    private AbstractAccountWidget ownerWidget;
+    private AbstractAccountWidget balanceWidget;
+    private AbstractAccountWidget changeWidget;
+
     public ControllerSummaryTab(ControllerScreen parent, ControllerContainer menu, int index, boolean isInventoryTab) {
         super("Summary", parent, menu, index, isInventoryTab);
+        initWidgets();
+    }
+
+    private void initWidgets() {
+        int y = 20;
+        int dy = 20;
+
+        ownerWidget = new OwnerWidget(getFont(),
+                getParent().getLeftPos() + 8, getParent().getTopPos() + y,
+                Component.literal(getMenu().getOwnerName()));
+
+        Long balance = getMenu().getValue();
+        String abbreviatedValue = GuiNumericUtil.abbreviate(balance);
+        balanceWidget = new BalanceWidget(getFont(),
+                getParent().getLeftPos() + 8, getParent().getTopPos() + y + dy,
+                Component.literal(abbreviatedValue));
+
+        Long change = getMenu().getValue();
+        String abbreviatedChange = GuiNumericUtil.abbreviate(change);
+        changeWidget = new ChangeWidget(getFont(),
+                getParent().getLeftPos() + 8, getParent().getTopPos() + y + (dy * 2),
+                Component.literal(abbreviatedChange));
+    }
+
+    @Override
+    public void init() {
+        registerWidgets();
+    }
+
+    protected void registerWidgets() {
+        getParent().registerWidget(ownerWidget);
+        getParent().registerWidget(balanceWidget);
+        getParent().registerWidget(changeWidget);
     }
 
 
@@ -33,13 +75,10 @@ public class ControllerSummaryTab extends ScreenTab<ControllerContainer, Control
             return;
         }
 
-
-        graphics.text(getFont(), Component.literal("Owner: " + owner), 10, 24, GuiElement.BASIC.getColor(), false);
-
-        getParent().formatAndDisplayValue(graphics, "Balance: ", getMenu().getValue(), 10, 34, mouseX, mouseY);
-        getParent().formatAndDisplayChange(graphics, 10, 44, mouseX, mouseY);
+        balanceWidget.setMessage(getMenu().getValue());
+        ownerWidget.setMessage(getMenu().getOwnerName());
+        changeWidget.setMessage(getMenu().getValueChange());
     }
-
 
     @Override
     public boolean handleScroll(double mouseX, double mouseY, double scrollX, double scrollY) {
@@ -48,11 +87,12 @@ public class ControllerSummaryTab extends ScreenTab<ControllerContainer, Control
 
     @Override
     public void onTabSwitch() {
+        registerWidgets();
     }
 
     @Override
     public void cleanup() {
-
+        getParent().clearWidgets();
     }
 
     @Override
