68af8d39e2347500f6e72ba4916fa6d634464da2
shiny <shinyepo42@gmail.com>
2026-04-28 19:45:32 +0200

    Cleanup MarketContainer

--- src/main/java/dev/shinyepo/resourcegenerator/menus/market/MarketContainer.java
+++ src/main/java/dev/shinyepo/resourcegenerator/menus/market/MarketContainer.java
@@ -3,26 +3,30 @@ package dev.shinyepo.resourcegenerator.menus.market;
 import dev.shinyepo.resourcegenerator.blocks.entities.MarketEntity;
 import dev.shinyepo.resourcegenerator.menus.types.AbstractContainerBase;
 import dev.shinyepo.resourcegenerator.registries.BlockRegistry;
+import dev.shinyepo.resourcegenerator.registries.DataComponentRegistry;
 import dev.shinyepo.resourcegenerator.registries.MenuRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 
+import java.util.UUID;
+
 public class MarketContainer extends AbstractContainerBase {
-    private MarketEntity marketEntity;
 
     public MarketContainer(int windowId, Player player, BlockPos pos) {
         super(MenuRegistry.MARKET_MENU.get(), windowId, pos, 1, 0, BlockRegistry.MARKET.get());
         if (player.level().getBlockEntity(pos) instanceof MarketEntity marketEntity) {
-            this.marketEntity = marketEntity;
 
             addSlot(marketEntity.getCardHandler(), 0, 252, 8);
-//            addSlot(marketEntity.getOutputHandler(), 0, 86, 36);
             layoutPlayerInventorySlots(player.getInventory(), 108, this.defaultInventoryY);
         }
     }
 
-    public String getOwnerName() {
-        return marketEntity.getOwnerName();
+    public UUID getOwnerId() {
+        var cardItem = this.getSlot(0).getItem();
+        if (cardItem.isEmpty()) return null;
+        var cardData = cardItem.get(DataComponentRegistry.ID_CARD);
+        if (cardData == null) return null;
+        return cardData.userId();
     }
 
 }
